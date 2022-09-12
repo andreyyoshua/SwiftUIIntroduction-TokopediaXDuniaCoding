dba0a362b6fc89f94acbd5ce868ac8bb5e181c11
Andrey Yoshua Manik <andrey.yoshua@gmail.com>
2022-09-12 09:52:39 +0700

    Initial Commit

---
+++ DuniaCoding-SwiftUI/DuniaCoding_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DuniaCoding_SwiftUIApp.swift
+//  DuniaCoding-SwiftUI
+//
+//  Created by Andrey Yoshua Manik on 12/09/22.
+//
+
+import SwiftUI
+
+@main
+struct DuniaCoding_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
