daeb195d1f0e484e2863ae83240f928ca9b093d0
gilang.bhagawadita <gilang.bhagawadita@tokopedia.com>
2022-09-29 23:37:43 +0700

    add image url helper

---
+++ DuniaCoding-SwiftUI/Helpers/Image+Url.swift
@@ -0,0 +1,21 @@
+//
+//  Image+Url.swift
+//  DuniaCoding-SwiftUI
+//
+//  Created by gilang.bhagawadita on 29/09/22.
+//
+
+import Foundation
+import SwiftUI
+
+extension Image {
+    func data(url: URL?) -> Self {
+        if let url = url {
+            if let data = try? Data(contentsOf: url) {
+                return Image(uiImage: UIImage(data: data)!)
+            }
+            return self
+        }
+        return self
+    }
+}
