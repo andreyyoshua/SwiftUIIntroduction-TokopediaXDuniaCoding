3242df9f64b1a2ae7ca75c1742bf21e35c7f16e4
gilang.bhagawadita <gilang.bhagawadita@tokopedia.com>
2022-09-30 17:05:59 +0700

    adjust intro

--- DuniaCoding-SwiftUI/Introduction/4. Function, Closure & Switch Case.playground/Contents.swift
+++ DuniaCoding-SwiftUI/Introduction/4. Function, Closure & Switch Case.playground/Contents.swift
@@ -24,44 +24,6 @@ func greet(_ person: String, on day: String) -> String {
 }
 greet("John", on: "Wednesday")
 
-func calculateStatistics(scores: [Int]) -> (min: Int, max: Int, sum: Int) {
-    var min = scores[0]
-    var max = scores[0]
-    var sum = 0
-
-    for score in scores {
-        if score > max {
-            max = score
-        } else if score < min {
-            min = score
-        }
-        sum += score
-    }
-
-    return (min, max, sum)
-}
-let statistics = calculateStatistics(scores: [5, 3, 100, 3, 9])
-print(statistics.sum)
-print(statistics.2)
-
-func returnFifteen() -> Int {
-    var y = 10
-    func add() {
-        y += 5
-    }
-    add()
-    return y
-}
-returnFifteen()
-
-func makeIncrementer() -> ((Int) -> Int) {
-    func addOne(number: Int) -> Int {
-        return 1 + number
-    }
-    return addOne
-}
-var increment = makeIncrementer()
-increment(7)
 
 func hasAnyMatches(list: [Int], condition: (Int) -> Bool) -> Bool {
     for item in list {
@@ -77,17 +39,9 @@ func lessThanTen(number: Int) -> Bool {
 var numbers = [20, 19, 7, 12]
 hasAnyMatches(list: numbers, condition: lessThanTen)
 
-numbers.map({ (number: Int) -> Int in
+
+numbers.map { number -> Int in
     let result = 3 * number
     return result
-})
-
-let mappedNumbers = numbers.map({ number in 3 * number })
-print(mappedNumbers)
-
-let sortedNumbers = numbers.sorted { firstIndex, nextIndex in
-    firstIndex > nextIndex
 }
-// let sortedNumbers = numbers.sorted { $0 > $1 }
-print(sortedNumbers)
 
--- DuniaCoding-SwiftUI/Introduction/7. Error Handling.playground/Contents.swift
+++ DuniaCoding-SwiftUI/Introduction/7. Error Handling.playground/Contents.swift
@@ -21,13 +21,3 @@ do {
     print(error)
 }
 
-do {
-    let printerResponse = try send(job: 1440, toPrinter: "Gutenberg")
-    print(printerResponse)
-} catch PrinterError.onFire {
-    print("I'll just put this over here, with the rest of the fire.")
-} catch let printerError as PrinterError {
-    print("Printer error: \(printerError).")
-} catch {
-    print(error)
-}
